99677e73d54424d42d09f5cba8bede101bd3c1c9
suren79thapa <thapasurendra447@gmail.com>
2025-10-28 22:19:53 +0545

    added url

--- src/api/apiUrl.js
+++ src/api/apiUrl.js
@@ -1,2 +1,3 @@
 // src/api/apiUrl.js
-export const baseUrl = "http://localhost:5000/api/students";
+// export const baseUrl = "http://localhost:5000/api/students";
+export const baseUrl = "https://managementbackened.onrender.com/api/students";
